a3b8220b3718981420d3a1499b8e94e3b8bcb2e4
clientworkaccess-cmd <clientworkaccess@gmail.com>
2025-11-06 22:49:24 +0500

    mongo uri secrete

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import { User } from "../models/user.js";
 
 const MONGO_URI = process.env.MONGODB_URI
-// "mongodb+srv://GavelDatabase:j5NmOUB8hi1LfBxI@gavelcluster.p7kueq8.mongodb.net/gavel?retryWrites=true&w=majority&appName=GavelCluster";
 
 let isConnected = false;
 export default async function dbConnection() {
