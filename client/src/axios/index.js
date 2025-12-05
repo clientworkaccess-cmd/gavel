e67cf27d4ec5fb89a6c2dbc17a673e1c10543019
clientworkaccess-cmd <clientworkaccess-cmd>
2025-12-05 21:48:16 +0500

    axios

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -5,7 +5,7 @@ import API_ENDPOINTS, { API_BASE_URL } from "../config/api";
 const apiClient = axios.create({
   baseURL: API_BASE_URL || "http://localhost:5000",
   withCredentials: true, 
-  timeout: 8000,
+  timeout: 40000,
   headers: {
     "Content-Type": "application/json",
   },
